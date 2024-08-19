3ad83a451ba6bff427ccf6562d3de22ce62b587b
mithilreddy369 <89974832+mithilreddy369@users.noreply.github.com>
2024-08-19 15:18:31 +0530

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -100,10 +100,7 @@ def explain_with_shap(instance, model, model_name):
     if explainer is not None:
         shap_values = explainer(instance)
         fig, ax = plt.subplots()
-        if model_name == "1Gradient Boosting  (Model Accuracy - 84.18%)":
-            shap.summary_plot(shap_values, features_df, plot_type="bar")
-        else:
-            shap.plots.waterfall(shap_values[0])
+        shap.plots.waterfall(shap_values[0])
         st.pyplot(fig)
     else:
         st.error("SHAP explainer not supported for this model.")
